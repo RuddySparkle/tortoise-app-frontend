3f789c1b5c196ff5cabaeb59eb7317956d0c0953
RuddySparkle <rutthee9999@gmail.com>
2024-04-06 23:28:18 +0700

    [TL] API for Pet Filtering

--- pet/services/api/v1/pets/type.ts
+++ pet/services/api/v1/pets/type.ts
@@ -1,73 +1,81 @@
 export interface IPetProfile {
-    id: string
-    name: string
-    price: number
-    category: string
-    media: string
-    seller_id: string
-    seller_name: string
-    seller_surname: string
+    id: string;
+    name: string;
+    price: number;
+    category: string;
+    media: string;
+    seller_id: string;
+    seller_name: string;
+    seller_surname: string;
 }
 
 export interface IPetDetail {
-    id: string
-    name: string
-    age: number
-    price: number
-    is_sold: boolean
-    description: string
-    weight: number
-    sex: string
-    species: string
-    category: string
-    behavior: string
-    media: string
-    medical_records: MedicalRecord[]
-    seller_id: string
+    id: string;
+    name: string;
+    age: number;
+    price: number;
+    is_sold: boolean;
+    description: string;
+    weight: number;
+    sex: string;
+    species: string;
+    category: string;
+    behavior: string;
+    media: string;
+    medical_records: MedicalRecord[];
+    seller_id: string;
 }
 
 export interface IPetUpdatePayload {
-    name: string
-    age: number
-    price: number
-    is_sold: boolean
-    description: string
-    weight: number
-    sex: string
-    species: string
-    category: string
-    behavior: string
-    media: string
-    medical_records: MedicalRecord[]
+    name: string;
+    age: number;
+    price: number;
+    is_sold: boolean;
+    description: string;
+    weight: number;
+    sex: string;
+    species: string;
+    category: string;
+    behavior: string;
+    media: string;
+    medical_records: MedicalRecord[];
 }
 
-export interface IPetQueryParams{
-    petId: string
+export interface IPetQueryParams {
+    petId: string;
 }
 
-export interface IPetUpdateParams{
-    petId: string,
-    payload: IPetUpdatePayload,
+export interface IPetUpdateParams {
+    petId: string;
+    payload: IPetUpdatePayload;
 }
 
-export interface IPetCreateParams{
-    sellerId: string,
-    payload: IPetUpdatePayload,
+export interface IPetCreateParams {
+    sellerId: string;
+    payload: IPetUpdatePayload;
 }
-  
+
 export interface MedicalRecord {
-    medical_id: string
-    medical_date: string
-    description: string
+    medical_id: string;
+    medical_date: string;
+    description: string;
 }
 
-export interface PetSearchParams{
-    search: string
-    filter: string
+export interface PetSearchParams {
+    category: string;
+    species: string;
+    sex: string;
+    behavior: string;
+    minAgeStr: string;
+    maxAgeStr: string;
+    minWeightStr: string;
+    maxWeightStr: string;
+    minPriceStr: string;
+    maxPriceStr: string;
 }
 export interface Pagination {
     total: number;
     limit: number;
     page: number;
     has_more: boolean;
-  }  
+}
