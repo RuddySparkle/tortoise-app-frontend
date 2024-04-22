c5da17d77d943a715f00f0942c2484497ccb20c4
Thammasorn Thammasarangkoon <poom99.2002@gmail.com>
2024-04-23 00:56:49 +0700

    Add Dockerfile

--- pet/services/clients/config.ts
+++ pet/services/clients/config.ts
@@ -2,7 +2,7 @@ import { AxiosRequestConfig } from 'axios';
 
 export const requestConfig: AxiosRequestConfig = {
   timeout: 60000,
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://35.171.56.239:8080',
 };
 
 export const DEFAULT_DEV_TOKEN = 'dev-token';
